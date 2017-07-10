ced9a1b4bc00992df69522625c50b0105e03e923
mengchao <mengchao@bis-soft.com.cn>
2017-07-10 13:30:57 +0800

    修改linux目录

--- logPath_linux.go
+++ logPath_linux.go
@@ -7,9 +7,14 @@ import (
 )
 
 func getLogFile() *os.File {
-	file, _ := exec.LookPath(os.Args[0])
-	filename := filepath.Base(file)
-	filename = `\PrintSystem\Log\` + filename + `.log`
+	logPath := `/opt/PrintSystem/Log/`
+	if _, err := os.Stat(logPath); err != nil && os.IsNotExist(err) {
+		os.MkdirAll(logPath, 0666)
+	}
+
+	filename, _ := exec.LookPath(os.Args[0])
+	filename = filepath.Base(filename)
+	filename = logPath + filename + `.log`
 
 	fileflag := os.O_CREATE | os.O_RDWR
 	if fi, err := os.Stat(filename); err == nil || (err != nil && os.IsExist(err)) {
